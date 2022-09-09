9957f79ef30716c51705a3998679ba3cda0cf3a5
Sasan Tazayoni <sasantazayoni@gmail.com>
2022-09-09 17:08:28 +0100

    JS functionality added for FAQ

--- script.js
+++ script.js
@@ -201,3 +201,18 @@ initializeAutoAdvance();
 
 /////////////////////////////////////////   FAQ PAGE   /////////////////////////////////////////
 
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    const btn = question.querySelector('.question-btn');
+
+    btn.addEventListener('click', function() {
+        questions.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+
+        question.classList.toggle('show-text');
+    });
+});
