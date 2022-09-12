9ac1919aa344efcfa40259d768a32a15e8fbd677
Sasan Tazayoni <sasantazayoni@gmail.com>
2022-09-12 15:47:19 +0100

    More classes refactored to suit BEM convention

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ initializeAutoAdvance();
 const questions = document.querySelectorAll('.question');
 
 questions.forEach(function(question) {
-    const btn = question.querySelector('.question-btn');
+    const btn = question.querySelector('.question__btn');
 
     btn.addEventListener('click', function() {
         questions.forEach(function(item) {
