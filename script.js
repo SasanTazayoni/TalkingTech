8e1326ee365077be17874606b186082f80850a00
Sasan Tazayoni <sasantazayoni@gmail.com>
2022-09-14 13:33:11 +0100

    Scroll to top button added for all pages

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/////////////////////////////////////////   ALL PAGES   /////////////////////////////////////////
+
+const navbar = document.querySelector('.navbar');
+const scrollToTop = document.querySelector('.scroll-up-link');
+
+window.addEventListener('scroll', function () {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > (navHeight * 2)) {
+        scrollToTop.classList.add('show-link');
+    } else {
+        scrollToTop.classList.remove('show-link');
+    }
+});
+
 /////////////////////////////////////////   HOME PAGE   /////////////////////////////////////////
 
 const buttons = document.querySelectorAll("[data-carousel-button");
@@ -205,6 +220,7 @@ initializeAutoAdvance();
 
 const questions = document.querySelectorAll('.question');
 
+/* Function to expand/collapse FAQ components */
 questions.forEach(function(question) {
     const btn = question.querySelector('.question__btn');
 
@@ -218,3 +234,4 @@ questions.forEach(function(question) {
         question.classList.toggle('show-text');
     });
 });
+
