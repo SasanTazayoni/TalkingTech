272585da85de8c61ac502253b4efddbb20daef74
Sasan Tazayoni <sasantazayoni@gmail.com>
2022-09-16 14:40:56 +0100

    Buggy JS for FAQ removed

--- scripts/faq.js
+++ scripts/faq.js
@@ -3,42 +3,55 @@
 const questions = document.querySelectorAll('.question');
 
 /* Function to expand/collapse FAQ components */
-questions.forEach(function(question) {
+questions.forEach(function (question) {
     const btn = question.querySelector('.question__btn');
 
     btn.addEventListener('click', function() {
-        questions.forEach(function(item) {
+        questions.forEach(function (item) {
             if (item !== question) {
-                toggleQuestion(item, true)
+                item.classList.remove('show-text');
             }
         });
-
-        toggleQuestion(question)
     });
 });
 
-/* Function to gradually collapse/expand FAQ content */
-function toggleQuestion (question, justClose = false) {
-    const opened = question.classList.contains('show-text');
+// /* Function to expand/collapse FAQ components */
+// questions.forEach(function(question) {
+//     const btn = question.querySelector('.question__btn');
 
-    if (justClose && !opened) return; 
+//     btn.addEventListener('click', function() {
+//         questions.forEach(function(item) {
+//             if (item !== question) {
+//                 toggleQuestion(item, true);
+//             }
+//         });
+
+//         toggleQuestion(question);
+//     });
+// });
+
+// /* Function to gradually collapse/expand FAQ content */
+// function toggleQuestion (question, justClose = false) {
+//     const opened = question.classList.contains('show-text');
+
+//     if (justClose && !opened) return; 
         
-    if (!opened) {
-        question.classList.add('show-text');
-    }
+//     if (!opened) {
+//         question.classList.add('show-text');
+//     }
 
-    const answer = question.querySelector('.answer__text p');
-    const answerHeight = answer.getBoundingClientRect().height
-    const finalHeight = opened ? 0 : answerHeight;
-    let height = opened ? answerHeight : 0;
-    if (!opened) answer.style.height = 0;
+//     const answer = question.querySelector('.answer__text p');
+//     const answerHeight = answer.getBoundingClientRect().height
+//     const finalHeight = opened ? 0 : answerHeight;
+//     let height = opened ? answerHeight : 0;
+//     if (!opened) answer.style.height = 0;
     
-    const interval = setInterval(() => {
-        answer.style.height = (opened ? --height : ++height) + "px";
-        if (height === finalHeight) {
-            clearInterval(interval);
-            answer.style.height = "auto";
-            if (opened) question.classList.remove('show-text');
-        }
-    }, 300 / finalHeight );
-}
+//     const interval = setInterval(() => {
+//         answer.style.height = (opened ? --height : ++height) + "px";
+//         if (height === finalHeight) {
+//             clearInterval(interval);
+//             answer.style.height = "auto";
+//             if (opened) question.classList.remove('show-text');
+//         }
+//     }, 300 / finalHeight );
+// }
