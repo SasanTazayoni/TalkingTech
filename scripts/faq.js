2ab3862ab1d6a6442bb9ae36b2941b6dee4f5d4e
Sasan Tazayoni <sasantazayoni@gmail.com>
2022-09-16 14:51:04 +0100

    FAQ bug fix (incorrect CSS selector)

--- scripts/faq.js
+++ scripts/faq.js
@@ -3,15 +3,17 @@
 const questions = document.querySelectorAll('.question');
 
 /* Function to expand/collapse FAQ components */
-questions.forEach(function (question) {
+questions.forEach(function(question) {
     const btn = question.querySelector('.question__btn');
 
     btn.addEventListener('click', function() {
-        questions.forEach(function (item) {
+        questions.forEach(function(item) {
             if (item !== question) {
                 item.classList.remove('show-text');
             }
         });
+
+        question.classList.toggle('show-text');
     });
 });
 
