e725ff80ca02821e92ac207200d51159ce95cd8a
Sasan Tazayoni <sasantazayoni@gmail.com>
2023-06-28 22:15:00 +0100

    Remove deprecated code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const navbar = document.querySelector('.navbar');
 const scrollToTop = document.querySelector('.scroll-up-link');
 
 window.addEventListener('scroll', function () {
-    const scrollHeight = window.pageYOffset;
+    const scrollHeight = window.YOffset;
     const navHeight = navbar.getBoundingClientRect().height;
     if (scrollHeight > (navHeight * 2)) {
         scrollToTop.classList.add('show-link');
