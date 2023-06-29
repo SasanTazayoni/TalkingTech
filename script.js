d5113d7459904cd7da177b20fd16f8e4f0d60636
Sasan Tazayoni <sasantazayoni@gmail.com>
2023-06-29 23:21:11 +0100

    Typo fix for scroll up button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const navbar = document.querySelector('.navbar');
 const scrollToTop = document.querySelector('.scroll-up-link');
 
 window.addEventListener('scroll', function () {
-    const scrollHeight = window.YOffset;
+    const scrollHeight = window.scrollY;
     const navHeight = navbar.getBoundingClientRect().height;
     if (scrollHeight > (navHeight * 2)) {
         scrollToTop.classList.add('show-link');
